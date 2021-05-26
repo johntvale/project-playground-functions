2c9aa7b5440106a8c5955c5bcbdcd996c6b31ec7
johntvale <johntvale@gmail.com>
2021-05-26 12:26:04 -0400

    Requisito 10 resolvido.

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,22 @@
 // Desafio 10
-function techList() {
+function techList(techs, name) {
   // seu código aqui
+
+  // ordenação do array techs e declaração do novo array que vai receber os objetos
+  let orderedList = techs.sort();
+  let techsAndNames = [];
+
+  if (orderedList.length > 0) { // verifica se o array techs é maior que 0
+    for (let index = 0; index < orderedList.length; index +=1) { // para cada posição do array recebido será atribuído um novo objeto, com uma key Tech[index] e uma key Name. Criando um Array de objetos.
+      techsAndNames.push({
+        tech: orderedList[index],
+        name: name,
+      });
+    }
+  } else {
+    return ('Vazio!');
+  }
+  return techsAndNames;
 }
 
 // Desafio 11
