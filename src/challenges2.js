e8e68bd028663e14dec5f11b77055402e7896c4b
johntvale <johntvale@gmail.com>
2021-05-28 12:19:32 -0400

    Requisito 11 resolvido.

--- src/challenges2.js
+++ src/challenges2.js
@@ -20,8 +20,41 @@ function techList(techs, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(numberList) {
+  if (numberList.length !== 11) { // verificar se o array tem lenght diferente 11
+      return 'Array com tamanho incorreto.';
+  }
+  
+  for (let index = 0; index < numberList.length; index +=1) { // verificar se o array tem número MENOR que 0 ou MAIOR que 9
+    let atual = numberList[index];
+    if (atual < 0 || atual > 9){
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+  }
+  
+  for (let index2 = 0; index2 < numberList.length; index2 +=1) { // Verificar se o array tem números que se repetem 3 vezes ou mais.
+    let pegaUmNumero = numberList[index2];
+    let contador = 0;
+    for (let forComparacao = 0; forComparacao < numberList.length; forComparacao += 1){
+      if (pegaUmNumero === numberList[forComparacao]){
+        contador += 1;
+      }
+    }
+    if (contador >= 3){
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+  }
+  // Principal ~> Organiza lista telefônica com parenteses e traço;  
+  let organizedPhoneNumber = '(' + numberList[0] + numberList[1] + ') '; // Recebe primeira parte: 2 dígitos.
+  
+  for(let index3 = 2; index3 < 7; index3 += 1) { // Recebe segunda parte: 5 dígitos.
+    organizedPhoneNumber += numberList[index3];
+  }
+  organizedPhoneNumber += '-';
+  for(let index4 = 7; index4 < 11; index4 += 1) { // recebe terceira parte, 4 últimos.
+    organizedPhoneNumber += numberList[index4];
+  }
+  return organizedPhoneNumber;
 }
 
 // Desafio 12
